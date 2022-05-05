8dda260cf60ef8f97e650fab98030fffcb079b8b
zhang11ca <7674207+zhang11ca@user.noreply.gitee.com>
2022-05-05 12:31:23 +0800

    newcss

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,34 +1,53 @@
 <script setup>
-const toJueJin = function(){
+import { ref } from 'vue'
+const e = ref('625013594@qq.com')
+const toJueJin = function () {
   window.open('https://juejin.cn/user/3897084295776302')
 }
-const toGit = function(){
+const toGit = function () {
   window.open('https://github.com/zhangjiadi225')
 }
-const toYuQue = function(){
+const toYuQue = function () {
   window.open('https://www.yuque.com/u25385678')
 }
-
-
+const email = function () {
+  const oInput = document.createElement('input')
+  oInput.value = e.value
+  document.body.appendChild(oInput)
+  oInput.select()
+  document.execCommand('Copy')
+  console.log('ok')
+  oInput.remove()
+}
+const file = function () {
+  window.open('/didiResume/new_file.html')
+}
 </script>
 
 <template>
   <div class="content">
-    <div class="content-item" @click="toJueJin">掘金</div>
-    <div class="content-item" @click="toGit">Github</div>
-    <div class="content-item" @click="toYuQue">语雀</div>
-    <div class="content-item" @click="weChat">微信 : 14735505218</div>
-    <div class="content-item" @click="email">邮箱 : 625013594@qq.com</div>
+    <div class="content-item two" @click="toJueJin">掘金</div>
+    <div class="content-item one" @click="toGit">Github</div>
+    <div class="content-item three" @click="toYuQue">语雀</div>
+    <div class="content-item showWechat" @click="weChat">
+      微信 : 14735505218
+    </div>
+    <div class="content-item showMessage" @click="email">
+      邮箱 :{{ e }}(<span style="color:#5db1b1">点击复制</span>)
+    </div>
+    <div class="content-item" @click="file">
+      精简版简历(<span style="color:#5db1b1">点击查看</span>)
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.content{
+.content {
   display: flex;
   justify-content: flex-start;
   align-items: center;
   flex-wrap: wrap;
-  &-item{
+  &-item {
     width: 300px;
     height: 100px;
     border: 1px solid #8329e2;
@@ -36,14 +55,52 @@ const toYuQue = function(){
     text-align: center;
     line-height: 100px;
     border-radius: 30px;
-    cursor:pointer;
-    color: orange;
+    cursor: pointer;
+    color: #ffffff;
+    letter-spacing: 1px;
   }
-  &-item:hover{
+  .showWechat:hover {
+    font-size: 0;
+    background-image: url(../assets/image/me.jpg);
+    background-repeat: no-repeat;
+    background-size: 80px 80px;
+    background-position: 50%;
+  }
+
+  .one {
+    color: #ffffff;
+  }
+
+  .two {
+    color: #ffffff;
+  }
+
+  .three {
+    color: #ffffff;
+  }
+
+  .one:hover {
+    background-color: #4cc9f0;
+    -webkit-box-shadow: 10px 10px 99px 6px rgba(76, 201, 240, 1);
+    -moz-box-shadow: 10px 10px 99px 6px rgba(76, 201, 240, 1);
+    box-shadow: 10px 10px 99px 6px rgba(76, 201, 240, 1);
     font-size: 20px;
-    color: brown;
+  }
+
+  .two:hover {
+    background-color: #f038ff;
+    -webkit-box-shadow: 10px 10px 99px 6px rgba(240, 56, 255, 1);
+    -moz-box-shadow: 10px 10px 99px 6px rgba(240, 56, 255, 1);
+    box-shadow: 10px 10px 99px 6px rgba(240, 56, 255, 1);
+    font-size: 20px;
+  }
+
+  .three:hover {
+    background-color: #b9e769;
+    -webkit-box-shadow: 10px 10px 99px 6px rgba(185, 231, 105, 1);
+    -moz-box-shadow: 10px 10px 99px 6px rgba(185, 231, 105, 1);
+    box-shadow: 10px 10px 99px 6px rgba(185, 231, 105, 1);
+    font-size: 20px;
   }
 }
-
-
 </style>
--- src/components/mini-part/describe.vue
+++ src/components/mini-part/describe.vue
@@ -9,17 +9,51 @@
 </template>
 
 <style lang="scss" scoped>
-.item{
+
+.item {
+    position: relative;
+    color: #dbf1ff;
     width: 780px;
     height: 40px;
-    margin-top: 20px;
+     margin-top: 20px;
     line-height: 40px;
+    letter-spacing:2px;
     margin-left: 40px;
-    border-radius: 15px;
-    border: 1px solid #555555;
+    border: 1px solid #669ad2;
+    cursor: pointer;
     padding-left: 10px;
     padding-right: 10px;
-    letter-spacing:2px;
-    color: #dbf1ff;
+    &::before,
+    &::after {
+        content: "";
+        position: absolute;
+        width: 40px;
+        height: 40px;
+        transition: .3s ease-in-out;
+    }
+    
+    &::before {
+        top: -5px;
+        left: -5px;
+        border-top: 4px solid #907ecd;
+        border-left: 4px solid #907ecd;
+    }
+    
+    &::after {
+        right: -5px;
+        bottom: -5px;
+        border-bottom: 4px solid #907ecd;
+        border-right: 4px solid #907ecd;
+    }
+    
+    &:hover::before,
+    &:hover::after {
+        width: calc(100% + 9px);
+        height: calc(100% + 9px);
+    }
 }
+.item:hover{
+  color: rgb(239, 48, 121);
+}
+
 </style>
